c01e12b21654bcb5d8ac78c5ce777c8b620e069b
Rithic Kumar <53635318+Rit-ctrl@users.noreply.github.com>
2020-12-04 11:46:44 +0530

    Add files via upload

---
+++ img_com.py
@@ -0,0 +1,56 @@
+import os 
+import sys 
+from PIL import Image 
+from easygui import *
+  
+# define a function for 
+# compressing an image 
+def compressMe(file, verbose = False,k=75): 
+    
+      # Get the path of the file 
+    # filepath = os.path.join(os.getcwd(),  
+    #                         file) 
+      
+    # open the image 
+    picture = Image.open(file) 
+    print(file)
+    # Save the picture with desired quality 
+    # To change the quality of image, 
+    # set the quality variable at 
+    # your desired level, The more  
+    # the value of quality variable  
+    # and lesser the compression 
+
+    saved=filesavebox()
+    picture.save(saved,  
+                 "JPEG",  
+                 optimize = True,  
+                 quality = k) 
+    return
+  
+# Define a main function 
+def main(): 
+    
+    verbose = False
+      
+    # checks for verbose flag 
+    if (len(sys.argv)>1): 
+        
+        if (sys.argv[1].lower()=="-v"): 
+            verbose = True       
+    
+    file = fileopenbox()
+    text="Enter quality factor 10 - 100"
+    title="Image compression"
+    d_int = 75
+    lower = 10
+    upper=100
+
+    output=integerbox(text,title,d_int,lower,upper)
+    compressMe(file,verbose,output)
+    
+    print("Done") 
+  
+# Driver code 
+if __name__ == "__main__": 
+    main() 
